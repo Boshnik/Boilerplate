3ae1c7c50772dc2c27b699c63d42522beeb598bd
Aleksandr Huz <Superboshnik@yandex.ru>
2019-06-16 16:03:51 +0300

    Fixed 'table' modifier

--- core/components/boilerplate/modifiers/table.php
+++ core/components/boilerplate/modifiers/table.php
@@ -1,23 +1,20 @@
 <?php
-/** @var modX $modx */
-if($modx->event->name == 'pdoToolsOnFenomInit') {
-    $fenom->addModifier('table', function ($arr,$class='') {
-        if(!is_array($arr)) return '';
-        
-        $counteTable = count($arr[0]);
-        foreach($arr as $idx => $item) {
-            if($idx == 0) {
-                $thead = "<thead><tr><th>" . implode("</th><th>",array_values($item)) . "</th></tr></thead>";
-                
-            } else {
-                if($counteTable != count($item)) {
-                    $item = array_pad($item,$counteTable,'');
-                }
-                $tbody .= "<tr><td>" . implode("</td><td>",array_values($item)) . "</td></tr>";
+$fenom->addModifier('table', function ($arr,$class='') {
+    if(!is_array($arr)) return '';
+    
+    $counteTable = count($arr[0]);
+    foreach($arr as $idx => $item) {
+        if($idx == 0) {
+            $thead = "<thead><tr><th>" . implode("</th><th>",array_values($item)) . "</th></tr></thead>";
+            
+        } else {
+            if($counteTable != count($item)) {
+                $item = array_pad($item,$counteTable,'');
             }
+            $tbody .= "<tr><td>" . implode("</td><td>",array_values($item)) . "</td></tr>";
         }
-        $tbody = "<tbody>$tbody</tbody>";
-        $output = empty($class) ? "<table>{$thead}{$tbody}</table>" : "<table class='$class'>{$thead}{$tbody}</table>";
-        return $output;
-    });    
-}
+    }
+    $tbody = "<tbody>$tbody</tbody>";
+    $output = empty($class) ? "<table>{$thead}{$tbody}</table>" : "<table class='$class'>{$thead}{$tbody}</table>";
+    return $output;
+});   
