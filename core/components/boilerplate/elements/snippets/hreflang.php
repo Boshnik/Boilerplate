98f408c8bf2d050ac2009ac1ad0e9e6f0d7b8b23
Aleksandr Huz <Superboshnik@yandex.ru>
2020-03-03 21:00:59 +0200

    Hreflang snippet fixed

--- core/components/boilerplate/elements/snippets/hreflang.php
+++ core/components/boilerplate/elements/snippets/hreflang.php
@@ -10,7 +10,7 @@ foreach($contexts as $context) {
     $ctx = $modx->getContext($context->key);
     $site_url = $ctx->getOption('site_url');
     $site_start = $ctx->getOption('site_start');
-    if($modx->resource->id != $site_start) {
+    if($modx->resource->uri != 'index') {
         $site_url .= $modx->resource->uri; 
     }
     $cultureKey = $ctx->getOption('cultureKey');
@@ -18,8 +18,8 @@ foreach($contexts as $context) {
         $parseTpl = str_replace('{$page}', $site_url, $tplDefault);
         $output .= $parseTpl;
     }
-	$parseTpl = str_replace('{$page}', $site_url, $tpl);
-	$parseTpl = str_replace('{$lang}', $cultureKey, $parseTpl);
-	$output .= $parseTpl;
+    $parseTpl = str_replace('{$page}', $site_url, $tpl);
+    $parseTpl = str_replace('{$lang}', $cultureKey, $parseTpl);
+    $output .= $parseTpl;
 }
 return $output;
