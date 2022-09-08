5595c8ad866b590b65f31be5d105dba7590246d7
Boshnik <Superboshnik@gmail.com>
2022-09-08 10:07:47 +0300

    Remove OnDocFormPrerender plugin

--- _build/elements/plugins.php
+++ _build/elements/plugins.php
@@ -8,7 +8,6 @@ return [
             'OnMODXInit' => [],
             'OnManagerPageInit' => [],
             'OnWebPagePrerender' => [],
-            'OnDocFormPrerender' => [],
             'OnFileManagerUpload' => [],
         ],
     ],
