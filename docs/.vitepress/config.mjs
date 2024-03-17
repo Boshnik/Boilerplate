876f0aa4dfadcd61bcfe7f67204dfd32555d33a3
Boshnik <Superboshnik@gmail.com>
2024-03-17 13:55:28 +0200

    changed target for home link [deploy]

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: 'https://boshnik.com/' },
+      { text: 'Home', link: 'https://boshnik.com/', target: '_self' },
       { text: 'Documentation', link: '/docs/' }
     ],
 
