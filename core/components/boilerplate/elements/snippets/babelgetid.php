688fdffbec7d492fa1963e6efeed7c47d32a511d
Boshnik <Superboshnik@gmail.com>
2024-09-22 23:29:57 +0300

    Fixed babelGetId snippet

--- core/components/boilerplate/elements/snippets/babelgetid.php
+++ core/components/boilerplate/elements/snippets/babelgetid.php
@@ -1,11 +1,15 @@
 <?php
-$tv = $modx->getObject(modTemplateVarResource::class, [
-    'tmplvarid' => 1,
-    'contentid' => $input,
-]);
+$name = $modx->getOption('babel.babelTvName', null, 'babelLanguageLinks', true);
+$tv = $modx->getObject(modTemplateVar::class, compact('name'));
 if (!$tv) return $input;
 
-$value = explode(';', $tv->value);
+$tvResource = $modx->getObject(modTemplateVarResource::class, [
+    'tmplvarid' => $tv->id,
+    'contentid' => $input,
+]);
+if (!$tvResource) return $input;
+
+$value = explode(';', $tvResource->value);
 foreach ($value as $v) {
     $v = explode(':', $v);
     if ($v[0] == $modx->resource->context_key) {
