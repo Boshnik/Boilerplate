6280b59b3d2bdd38fc18c5e2ff53232b74288245
Boshnik <Superboshnik@gmail.com>
2024-09-23 15:14:10 +0300

    improved encrypt && decrypt snippets

--- core/components/boilerplate/elements/snippets/decrypt.php
+++ core/components/boilerplate/elements/snippets/decrypt.php
@@ -1,14 +1,24 @@
 <?php
 
 $key = md5($modx->uuid);
-$input = str_replace(['-', '_'], ['+', '/'], $input);
+$input = str_replace('_', '/', $input);
 
 list($encryptedData, $iv) = explode('::', base64_decode($input), 2);
 $decryptedData = openssl_decrypt($encryptedData, 'aes-256-cbc', $key, 0, $iv);
 
 if (strpos($decryptedData, '::') !== false) {
-    list($data, $timestamp) = explode('::', $decryptedData);
-    return (time() > $timestamp) ? '' : $data;
+    $parts = explode('::', $decryptedData);
+    if (count($parts) === 2) {
+        list($data, $timestamp) = $parts;
+        if (time() > $timestamp) {
+            return '';
+        }
+    }
+    $data = $parts[0];
+} else {
+    $data = $decryptedData;
 }
 
-return $decryptedData;
+$decodedData = json_decode($data, true);
+
+return (json_last_error() === JSON_ERROR_NONE) ? $decodedData : $data;
--- core/components/boilerplate/elements/snippets/encrypt.php
+++ core/components/boilerplate/elements/snippets/encrypt.php
@@ -3,10 +3,20 @@
 $key = md5($modx->uuid);
 $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-256-cbc'));
 
-$timestamp = isset($expiration) ? time() + $expiration : null;
+if ($iv === false) {
+    return 'Error generating IV';
+}
+
+$expiration = $expiration ?? 0;
+$timestamp = $expiration > 0 ? time() + $expiration : null;
+
+if (is_array($input)) {
+    $input = json_encode($input);
+}
+
 $input .= $timestamp ? '::' . $timestamp : '';
 
 $encryptedData = openssl_encrypt($input, 'aes-256-cbc', $key, 0, $iv);
 $base64EncryptedData = base64_encode($encryptedData . '::' . $iv);
 
-return str_replace(['+', '/', '='], ['-', '_', ''], $base64EncryptedData);
+return str_replace('/', '_', $base64EncryptedData);
